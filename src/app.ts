881807bf9df737974b1edf3f8c29ea015871ba36
ammar <ammarraneez@gmail.com>
2021-11-03 09:43:19 +0530

    feat: get form field values

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,61 @@
-// Code goes here!
+// autobind decorator
+function autobind(
+  _: any,
+  _2: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  }
+
+  return adjDescriptor;
+}
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+
+  private configure() {
+    // specify that the 'this' will refer to 'this' of this current function
+    this.element.addEventListener('submit', this.submitHandler);
+  }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+
+    // else, the 'this' would refer to this function
+    console.log(this.titleInputElement.value);
+  }
+}
+
+const prjInput = new ProjectInput();
